0e0604a09670f2e66c0fc15bcbe4c2c9cefe44cf
Sai Charitha Akula <charithaakula1996@gmail.com>
2023-04-10 06:28:24 -0400

    code cleanup

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -11,7 +11,9 @@ import sys
 from typing import Iterable
 
 import torch
+
 import utils
+
 
 def train_one_epoch(model: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
@@ -68,3 +70,65 @@ def train_one_epoch(model: torch.nn.Module,
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+def train_one_epoch_clip(model: torch.nn.Module,
+                    teacher: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler,
+                    log_writer=None,
+                    args=None):
+    model.train(True)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+
+    update_freq = args.update_freq
+
+    optimizer.zero_grad()
+    #for data_iter_step, (samples, images) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):    
+    for data_iter_step, (batch, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+        samples, images = batch #vit, clip
+         # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % update_freq == 0:
+           utils.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        if not isinstance(samples, list):
+            samples = samples.to(device, non_blocking=True)
+        if not isinstance(images, list):
+            images = images.to(device, non_blocking=True)            
+        labels = None
+        with torch.no_grad(), torch.cuda.amp.autocast():
+            clip_features = teacher.infer_image({"image": [images]})         
+        loss, _, _ = model(samples, clip_features, labels, args.mask_ratio )
+
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+    
+        loss /= update_freq
+        loss_scaler(loss, optimizer, parameters=model.parameters(),
+                    update_grad=(data_iter_step + 1) % update_freq == 0)
+        if (data_iter_step + 1) % update_freq == 0:
+            optimizer.zero_grad()
+            torch.cuda.empty_cache() # clear the GPU cache at a regular interval for training ME network
+        
+        metric_logger.update(loss=loss_value)
+
+        lr = optimizer.param_groups[0]["lr"]
+        metric_logger.update(lr=lr)
+        
+        loss_value_reduce = utils.all_reduce_mean(loss_value)
+        if log_writer is not None and (data_iter_step + 1) % update_freq == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.update(train_loss=loss_value_reduce, head="loss", step=epoch_1000x)
+            log_writer.update(lr=lr, head="opt", step=epoch_1000x)
+    
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}    
