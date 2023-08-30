0220f2ef998d7f7fed780e39dc4d68193c60d481
Luca <Luca.Rettenberger@kit.edu>
2023-08-30 16:27:31 +0200

    sup

---
+++ hpc_jobs/horeka_sweep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --partition=accelerated
+#SBATCH --time=31:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=luca.rettenberger@kit.edu
+#SBATCH --error=%j_error.txt
+#SBATCH --output=%j_output.txt
+#SBATCH --job-name=hyperparam-sem-seg
+
+export SWEEPID="lucare/sem-segmentation/nrrcv611"
+
+# aendern?
+NUM_COUNTS=1
+
+# remove all modules
+module purge
+
+# activate cuda
+module load devel/cuda/11.1
+
+# activate conda env
+source /home/hk-project-test-dl4pm/hgf_xda8301/miniconda3/etc/profile.d/conda.sh
+conda activate sem-segmentation
+
+
+cd /home/hk-project-test-dl4pm/hgf_xda8301/ConvNeXt-V2
+
+
+# start train
+CUDA_VISIBLE_DEVICES=0 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=1 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=2 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=3 wandb agent $SWEEPID
+
+wait < <(jobs -p)
